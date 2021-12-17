d1128e8adda47ca6cfb5c262719be7ddcb2f9673
manasseh-fikadu <80324103+manasseh-fikadu@users.noreply.github.com>
2021-12-17 17:48:57 +0300

    Create SortColors.py

---
+++ week2/day3/SortColors.py
@@ -0,0 +1,12 @@
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        temp = 0
+        for i in range(0, len(nums)):
+            for j in range(0, len(nums) - 1):
+                if(nums[j] > nums[j + 1]):
+                    temp = nums[j]
+                    nums[j] = nums[j+1]
+                    nums[j + 1] = temp
