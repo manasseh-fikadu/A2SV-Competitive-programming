a846f86c315140a9269120767575b156a39074b1
manasseh-fikadu <80324103+manasseh-fikadu@users.noreply.github.com>
2021-12-18 09:24:20 +0300

    Create FindOriginalArray.py

---
+++ week2/day3/FindOriginalArray.py
@@ -0,0 +1,17 @@
+class Solution:
+    def findOriginalArray(self, changed: List[int]) -> List[int]:
+        changed.sort()
+        original = []
+        s = Counter(changed)
+        for i in range(0, len(changed)):
+            if changed[i] in s and (2*changed[i]) in s:
+                original.append(changed[i])
+                s[changed[i]] -= 1
+                if s[changed[i]] == 0:
+                    s.pop(changed[i])
+                
+                s[changed[i]*2] -= 1
+                if s[changed[i]*2] == 0:
+                    s.pop(changed[i]*2)
+        if not s:
+            return original
