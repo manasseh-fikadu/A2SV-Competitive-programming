824202a471095c530585b05d387570464e628ea4
manasseh-fikadu <80324103+manasseh-fikadu@users.noreply.github.com>
2021-12-20 19:07:35 +0300

    Create NotEqualToNeighbor.py

---
+++ week2/day4/NotEqualToNeighbor.py
@@ -0,0 +1,10 @@
+class Solution:
+    def rearrangeArray(self, nums: List[int]) -> List[int]:
+        for i in range(1, len(nums) -1):
+            pre = nums[i-1]
+            current = nums[i]
+            next = nums[i+1]
+            
+            if (pre < current < next) or (pre > current > next):
+                nums[i+1], nums[i] = nums[i], nums[i+1]
+        return nums
