96e429a07a4d8dab9e7952e8075338281654b767
manasseh-fikadu <80324103+manasseh-fikadu@users.noreply.github.com>
2022-01-22 10:17:31 +0300

    Added a Monotonic Stack Solution alternative

--- Stacks & Queues/Stacks/nextGreaterElement.py
+++ Stacks & Queues/Stacks/nextGreaterElement.py
@@ -1,5 +1,6 @@
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        # Brute Force way
         stack = [-1] * len(nums1)
         
         for i in range(len(nums1)):
@@ -10,3 +11,15 @@ class Solution:
                     break
                     
         return stack
+    
+    
+    # Updated with a Monotonic Stack Context
+    greater = {}
+        stack = []
+        
+        for num in nums2:
+            while stack and stack[-1] < num:
+                greater[stack.pop()] = num
+            stack.append(num)
+            
+        return [greater.get(num, -1) for num in nums1]
