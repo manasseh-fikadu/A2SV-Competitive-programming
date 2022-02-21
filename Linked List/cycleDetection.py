6304a36088bec62305533ef4eb3fdce9bb249367
manasseh-fikadu <80324103+manasseh-fikadu@users.noreply.github.com>
2022-02-21 22:17:04 +0300

    Create cycleDetection.py

---
+++ Linked List/cycleDetection.py
@@ -0,0 +1,14 @@
+def has_cycle(head):
+    if (head == None):
+        return False
+    else:
+        slow = head
+        fast = head.next
+        while (slow != fast) :
+            if (fast == None or fast.next == None):
+                return False
+            else:
+                slow = slow.next;
+                fast = fast.next.next;
+        return True
+if __name__ == '__main
