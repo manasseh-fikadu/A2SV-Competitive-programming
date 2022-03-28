5d73e7a44d0e791a0404509374c60117b1e778d4
manasseh-fikadu <80324103+manasseh-fikadu@users.noreply.github.com>
2022-03-28 13:11:43 +0300

    Time: 44 ms (100.00%), Space: 26 MB (69.71%) - LeetHub

--- 187-repeated-dna-sequences/187-repeated-dna-sequences.py
+++ 187-repeated-dna-sequences/187-repeated-dna-sequences.py
@@ -2,7 +2,7 @@ class Solution:
     def findRepeatedDnaSequences(self, s: str) -> List[str]:
         visited, store = set(), set()
         
-        for i in range(len(s) - 9):
+        for i in range(len(s)):
             curr = s[i:i+10]
             if curr in visited:
                 store.add(curr)
