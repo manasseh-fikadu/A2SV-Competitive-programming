634f23eda4894008581b41450b0fae11da9f182e
manasseh-fikadu <80324103+manasseh-fikadu@users.noreply.github.com>
2022-04-07 17:21:33 +0300

    Time: 56 ms (14.75%), Space: 13.9 MB (69.14%) - LeetHub

---
+++ 1328-break-a-palindrome/1328-break-a-palindrome.py
@@ -0,0 +1,23 @@
+class Solution:
+    def breakPalindrome(self, palindrome: str) -> str:
+        palindrome = list(palindrome)
+        counts = Counter(palindrome)
+        n = len(palindrome)
+        flag = False
+        
+        if n <= 1:
+            return ""
+        
+        if 'a' in counts and len(palindrome) - counts['a'] in {0,1}:
+            palindrome[-1] = 'b'
+            return ''.join(palindrome)
+        
+        for i in range(n):
+            if palindrome[i] != 'a':
+                palindrome[i] = 'a'
+                return ''.join(palindrome)
+            
+           
+        
+    
+    
