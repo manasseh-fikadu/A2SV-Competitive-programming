598eb8a2feb358beb40994a93cdd48d0cee2a68d
manasseh-fikadu <80324103+manasseh-fikadu@users.noreply.github.com>
2022-05-13 18:23:30 +0300

    Time: 45 ms (50.86%), Space: 13.9 MB (50.47%) - LeetHub

--- 190-reverse-bits/190-reverse-bits.py
+++ 190-reverse-bits/190-reverse-bits.py
@@ -1,9 +1,17 @@
 class Solution:
     def reverseBits(self, n: int) -> int:
-        res = 0
         
-        for i in range(32):
-            bit = (n >> i) & 1
-            res = res | (bit << (31 - i))
+        def binary2int(binary): 
+            int_val, i, n = 0, 0, 0
+            while(binary != 0): 
+                a = binary % 10
+                int_val = int_val + a * pow(2, i) 
+                binary = binary//10
+                i += 1
+            return int_val
             
-        return res
+        res = ''
+        for i in range(32):
+            flip = (n >> i) & 1
+            res += str(flip)
+        return binary2int(int(res))
