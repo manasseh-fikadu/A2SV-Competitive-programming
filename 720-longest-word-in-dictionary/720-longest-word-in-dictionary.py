55bfbe7b225520f117543e2df2efdc39813a9dde
manasseh-fikadu <80324103+manasseh-fikadu@users.noreply.github.com>
2022-07-07 18:05:00 +0300

    Time: 278 ms (21.71%), Space: 15.4 MB (32.15%) - LeetHub

--- 720-longest-word-in-dictionary/720-longest-word-in-dictionary.py
+++ 720-longest-word-in-dictionary/720-longest-word-in-dictionary.py
@@ -1,11 +1,36 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.endOfWord = False
+
 class Solution:
     def longestWord(self, words: List[str]) -> str:
-        words.sort()
-        ans = ''
-        contains = set([''])
+        root = TrieNode()
+
+        def insert(word):
+            curr = root
+            for char in word:
+                if char not in curr.children:
+                    curr.children[char] = TrieNode()
+                curr = curr.children[char]
+            curr.endOfWord = True
+            
         for word in words:
-            if word[:-1] in contains:
-                contains.add(word)
-                if len(word) > len(ans):
-                    ans = word
-        return ans
+            insert(word)
+            
+        res = ''
+        
+        for word in words:
+            if len(word) < len(res): continue
+
+            curr = root
+
+            for letter in word:
+                curr = curr.children[letter]
+                if not curr.endOfWord: break
+            
+            if curr.endOfWord:
+                if (len(word) > len(res) or (len(word) == len(res) and word < res)):
+                    res = word        
+            
+        return res
