59e6a50cf777dfb266a6ee3d215ea848519f6b03
manasseh-fikadu <80324103+manasseh-fikadu@users.noreply.github.com>
2022-07-13 15:13:45 +0300

    Time: 1010 ms (50.69%), Space: 22.3 MB (19.95%) - LeetHub

--- 802-find-eventual-safe-states/802-find-eventual-safe-states.py
+++ 802-find-eventual-safe-states/802-find-eventual-safe-states.py
@@ -1,20 +1,25 @@
 class Solution:
     def eventualSafeNodes(self, graph: List[List[int]]) -> List[int]:
-        safe = defaultdict()
-
-        def dfs(i):
-            if i in safe:
-                return safe[i]
-            safe[i] = False
-            for nei in graph[i]:
-                if not dfs(nei):
-                    return safe[i]
-            safe[i] = True
-            return safe[i]            
-
-        ans = []
-        for i in range(len(graph)):
-            if dfs(i):
-                ans.append(i)
-        return ans
+        adj_lst = {}
+        outcome_count = {}
+        queue = deque()
+        res = []
+        
+        for i, nei in enumerate(graph):
+            outcome_count[i] = len(nei)
+            if not outcome_count[i]:
+                queue.append(i)
+            for j in nei:
+                adj_lst.setdefault(j, []).append(i)
+                
+        while queue:
+            node = queue.popleft()
+            res.append(node)
+            for nei in adj_lst.get(node, []):
+                outcome_count[nei] -= 1
+                if not outcome_count[nei]:
+                    queue.append(nei)
+        
+        res.sort()
+        return res
         
