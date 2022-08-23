68ae4c2af8d412247f99fcb71a15f20383f35dce
manasseh-fikadu <80324103+manasseh-fikadu@users.noreply.github.com>
2022-08-23 17:47:44 +0300

    Time: 207 ms (59.70%), Space: 15.1 MB (98.64%) - LeetHub

--- 268-missing-number/268-missing-number.py
+++ 268-missing-number/268-missing-number.py
@@ -1,10 +1,19 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        _set = set(nums)
-        n = max(nums)
+        # O(N) space solution
+#         _set = set(nums)
+#         n = max(nums)
         
-        for i in range(n):
-            if i not in _set:
-                return i
+#         for i in range(n):
+#             if i not in _set:
+#                 return i
             
-        return n + 1
+#         return n + 1
+
+        # O(1) space solution
+        res = len(nums)
+
+        for i in range(len(nums)):
+            res += i - nums[i]
+        return res
+        
