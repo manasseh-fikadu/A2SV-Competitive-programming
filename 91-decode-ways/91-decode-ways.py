21b143a33f9dfd198f64e1d4d8162560e0469c17
Minase Fikadu <80324103+manasseh-fikadu@users.noreply.github.com>
2022-10-01 10:47:00 +0300

    Time: 68 ms (20.37%), Space: 14.2 MB (10.63%) - LeetHub

--- 91-decode-ways/91-decode-ways.py
+++ 91-decode-ways/91-decode-ways.py
@@ -1,6 +1,7 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
         dp = { len(s) : 1 }
+
         def dfs(i):
             if i in dp:
                 return dp[i]
@@ -11,6 +12,8 @@ class Solution:
             if (i + 1 < len(s) and (s[i] == "1" or s[i] == "2" and s[i + 1] in "0123456")):
                 res += dfs(i + 2)
             dp[i] = res
+            
             return res
+        
         return dfs(0)
                 
