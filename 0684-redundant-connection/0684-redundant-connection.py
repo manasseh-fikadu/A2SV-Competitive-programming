be6c2d40b5634fd37cbfb71a9e6b637d28049c31
Minase Fikadu <80324103+manasseh-fikadu@users.noreply.github.com>
2022-10-12 12:04:51 +0300

    Time: 200 ms (5.21%), Space: 14.4 MB (56.27%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.py
+++ 0684-redundant-connection/0684-redundant-connection.py
@@ -1,21 +1,24 @@
 class Solution:
     def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
-#         DFS Implementation
+#       BFS Implementation
         graph = defaultdict(list)
         for u, v in edges:
-            if self.dfs(graph, u, v, set()):
+            if self.bfs(graph, u, v):
                 return [u, v]
             graph[u].append(v)
             graph[v].append(u)
         return []
 
-    def dfs(self, graph, u, v, visited):
+    def bfs(self, graph, u, v):
         if u not in graph:
             return False
-        if u in visited:
-            return False
-        visited.add(u)
-        if u == v:
-            return True
-        return any(self.dfs(graph, nei, v, visited) for nei in graph[u])
+        q = [u]
+        visited = set()
+        while q:
+            node = q.pop(0)
+            if node == v:
+                return True
+            visited.add(node)
+            q.extend([nei for nei in graph[node] if nei not in visited])
+        return False
 
