48be193acb427aad4d8305fae71c47b626a30162
Minase Fikadu <80324103+manasseh-fikadu@users.noreply.github.com>
2022-10-23 06:50:59 +0300

    Time: 198 ms (95.75%), Space: 15.8 MB (60.69%) - LeetHub

---
+++ 0645-set-mismatch/0645-set-mismatch.py
@@ -0,0 +1,8 @@
+class Solution:
+    def findErrorNums(self, nums: List[int]) -> List[int]:
+        res = sum(set(nums))
+        non_missing = sum(nums) - res
+        missing = sum(range(1,len(nums)+1)) - res
+        return [non_missing, missing]
+                
+        
