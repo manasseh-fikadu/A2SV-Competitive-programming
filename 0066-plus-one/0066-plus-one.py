fb13c29319013f15e815e3f1f421375e4ef861a8
Minase Fikadu <80324103+manasseh-fikadu@users.noreply.github.com>
2022-11-10 12:27:10 +0300

    Time: 42 ms (81.81%), Space: 13.9 MB (11.89%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.py
@@ -0,0 +1,6 @@
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        for i in range(len(digits)):
+            digits[i] = str(digits[i])
+            
+        return list(str(int("".join(digits)) + 1))
