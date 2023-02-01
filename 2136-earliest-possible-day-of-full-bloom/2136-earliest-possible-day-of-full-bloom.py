b03d788bb699481808e213811b493fc086854ed3
Minase Fikadu <80324103+manasseh-fikadu@users.noreply.github.com>
2023-02-01 10:33:31 +0300

    Time: 1748 ms (84.16%), Space: 31.5 MB (65.53%) - LeetHub

--- 2136-earliest-possible-day-of-full-bloom/2136-earliest-possible-day-of-full-bloom.py
+++ 2136-earliest-possible-day-of-full-bloom/2136-earliest-possible-day-of-full-bloom.py
@@ -2,8 +2,8 @@ class Solution:
     def earliestFullBloom(self, plantTime: List[int], growTime: List[int]) -> int:
         curr_time = 0
         result = 0
-        seeds = sorted(range(len(plantTime)), key=lambda x: -growTime[x]) 
-        for seed in seeds:
+        index = sorted(range(len(plantTime)), key=lambda x: -growTime[x]) 
+        for seed in index:
             curr_time += plantTime[seed]
             result = max(result, curr_time + growTime[seed])
         return result
