841f681ae459dcec7bdb26252bb0116a34172ae7
Minase Fikadu <80324103+manasseh-fikadu@users.noreply.github.com>
2023-02-07 22:55:39 +0300

    Time: 24 ms (97.37%), Space: 13.8 MB (87.73%) - LeetHub

---
+++ 0068-text-justification/0068-text-justification.py
@@ -0,0 +1,31 @@
+class Solution:
+    def fullJustify(self, words: List[str], L: int) -> List[str]:
+        if len(words) == 0:
+            return []
+        if len(words) == 1:
+            return [words[0] + ' ' * (L - len(words[0]))]
+        result = []
+        i = 0
+        while i < len(words):
+            line = []
+            line.append(words[i])
+            i += 1
+            while i < len(words) and len(' '.join(line)) + len(words[i]) + 1 <= L:
+                line.append(words[i])
+                i += 1
+            if i == len(words):
+                result.append(' '.join(line) + ' ' * (L - len(' '.join(line))))
+            else:
+                if len(line) == 1:
+                    result.append(line[0] + ' ' * (L - len(line[0])))
+                else:
+                    space = L - len(''.join(line))
+                    space_num = len(line) - 1
+                    space_each = space // space_num
+                    space_left = space % space_num
+                    for j in range(space_left):
+                        line[j] += ' ' * (space_each + 1)
+                    for j in range(space_left, space_num):
+                        line[j] += ' ' * space_each
+                    result.append(''.join(line))
+        return result
