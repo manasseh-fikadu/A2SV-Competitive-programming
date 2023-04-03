a5b3eff39efa913063324cdb2cf605400c76cb4f
Minase Fikadu <80324103+manasseh-fikadu@users.noreply.github.com>
2023-04-04 00:51:00 +0300

    Time: 6082 ms (31.78%), Space: 27.8 MB (100.00%) - LeetHub

--- 1649-create-sorted-array-through-instructions/1649-create-sorted-array-through-instructions.py
+++ 1649-create-sorted-array-through-instructions/1649-create-sorted-array-through-instructions.py
@@ -1,16 +1,29 @@
-from sortedcontainers import SortedList
+class BinaryIndexedTree:
+    def __init__(self, n: int):
+        self.tree = [0] * (n + 1)
+
+    def update(self, i: int, delta: int):
+        while i < len(self.tree):
+            self.tree[i] += delta
+            i += i & (-i)
+
+    def query(self, i: int) -> int:
+        count = 0
+        while i > 0:
+            count += self.tree[i]
+            i -= i & (-i)
+        return count    
 
 class Solution:
     def createSortedArray(self, instructions: List[int]) -> int:
-        list_ = SortedList()
-        counts = defaultdict(int)
-        ans = 0
-        
-        for i in range(len(instructions)):
-            list_.add((instructions[i], i))
-            position = list_.index((instructions[i], i))
-            ans += min(position - counts[instructions[i]], i - position) % (10**9 + 7)
-            counts[instructions[i]] += 1
-            
-        return ans % (10**9 + 7)
+        BIT_MAX = 10 ** 5 + 1
+        MODULO = 10 ** 9 + 7
+        bit = BinaryIndexedTree(BIT_MAX)
+        cost = 0
+        for i, x in enumerate(instructions):
+            less_than = bit.query(x - 1)
+            greater_than = i - bit.query(x)
+            cost += min(less_than, greater_than)
+            bit.update(x, 1)
+        return cost % MODULO
         
