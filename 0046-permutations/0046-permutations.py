7d75f7ce7b6efb098f740654562a8af0c53ffdca
Minase Fikadu <80324103+manasseh-fikadu@users.noreply.github.com>
2023-04-05 23:48:30 +0300

    Time: 40 ms (71.79%), Space: 14.1 MB (49.56%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,18 +1,21 @@
 class Solution:
-    def permute(self, a: List[int]) -> List[List[int]]:
-        n = len(a)
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        n = len(nums)
         target = (1 << n) - 1
         res = []
-        v = []
-        def recur(mask):
-            nonlocal res, v
+        intermediate = []
+        
+        def helper(mask):
+            nonlocal res, intermediate
             if target == mask:
-                res.append(list(v))
+                res.append(list(intermediate))
                 return
             for i in range(n):
                 if (mask & (1 << i)) == 0:
-                    v.append(a[i])
-                    recur(mask | (1 << i))
-                    v.pop()
-        recur(0)
+                    intermediate.append(nums[i])
+                    helper(mask | (1 << i))
+                    intermediate.pop()
+                    
+        helper(0)
         return res
+        
