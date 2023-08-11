f200dd00af749f0ecb0648d40587c9b2c17addda
Minase Fikadu <80324103+manasseh-fikadu@users.noreply.github.com>
2023-08-11 12:59:18 +0300

    Time: 51 ms (96.95%), Space: 17 MB (11.95%) - LeetHub

---
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.py
@@ -0,0 +1,39 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> bool:
+        n = len(nums)
+        if n == 0: return False
+        end, start = n - 1, 0
+
+        while start <= end:
+            mid = (start + end) // 2
+
+            if nums[mid] == target:
+                return True
+
+            if not self.isBinarySearchHelpful(nums, start, nums[mid]):
+                start += 1
+                continue
+
+            pivotArray = self.existsInFirst(nums, start, nums[mid])
+
+            targetArray = self.existsInFirst(nums, start, target)
+
+            if pivotArray ^ targetArray:
+                if pivotArray:
+                    start = mid + 1
+                else:
+                    end = mid - 1
+
+            else:
+                if nums[mid] < target:
+                    start = mid + 1
+                else:
+                    end = mid - 1
+
+        return False
+
+    def isBinarySearchHelpful(self, nums, start, element):
+        return nums[start] != element
+
+    def existsInFirst(self, nums, start, element):
+        return nums[start] <= element
