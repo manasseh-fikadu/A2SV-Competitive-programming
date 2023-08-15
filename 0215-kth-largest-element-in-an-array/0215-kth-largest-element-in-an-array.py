bc9b109ebd6c6007abc7baa92a5bd3a8905a6d1f
Minase Fikadu <80324103+manasseh-fikadu@users.noreply.github.com>
2023-08-15 09:14:37 +0300

    Time: 434 ms (93.11%), Space: 29.5 MB (54.97%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
@@ -4,11 +4,12 @@ class Solution:
             return None
 
         minheap = []
+
         for num in nums:
             if len(minheap) < k:
                 heapq.heappush(minheap, num)
-            else:
-                if num > minheap[0]:
-                    heapq.heappop(minheap)
-                    heapq.heappush(minheap, num)
+            elif num > minheap[0]:
+                heapq.heappop(minheap)
+                heapq.heappush(minheap, num)
+
         return minheap[0]
